df2b665aa96f6f9ccb32c39a94761d8974f7851e
Jonathan Martin <me@nybblr.com>
2013-04-10 12:49:40 -0400

    Implemented GetDeriv for RotateEuler.

--- RotateEuler.cpp
+++ RotateEuler.cpp
@@ -14,7 +14,7 @@ static double TORADIANS( double degrees )
   return (degrees * 3.14159) / 180.0;
 }
 
-RotateEuler::RotateEuler( int axis, double angle, double min, double max, char* name, DofList& dofs ) : 
+RotateEuler::RotateEuler( int axis, double angle, double min, double max, char* name, DofList& dofs ) :
   mAxis(0,0,0)
 {
   mIndex = 1;
@@ -61,7 +61,7 @@ RotateEuler::RotateEuler( istream& in, DofList* dofs ) : mAxis(0,0,0)
 
 void RotateEuler::Apply()
 {
-  glRotated(180.0 * mAngle / 3.14159, mAxis[0], mAxis[1], mAxis[2] ); 
+  glRotated(180.0 * mAngle / 3.14159, mAxis[0], mAxis[1], mAxis[2] );
 }
 
 Mat4d RotateEuler::GetTransform()
@@ -82,63 +82,131 @@ Mat4d RotateEuler::GetTransform()
   // calculate coefficients in !row! major order
   switch(axis)
     {	case 0:
-	  m[0][0] = 1;
-	  m[0][1] = 0;
-	  m[0][2] = 0;
- 
-	  m[1][0] = 0;
-	  m[1][1] = cosAngle;
-	  m[1][2] = -sinAngle;
-			
+    m[0][0] = 1;
+    m[0][1] = 0;
+    m[0][2] = 0;
 
-	  m[2][0] = 0;
-	  m[2][1] = sinAngle;
-	  m[2][2] = cosAngle;
-	  break;
+    m[1][0] = 0;
+    m[1][1] = cosAngle;
+    m[1][2] = -sinAngle;
+
+
+    m[2][0] = 0;
+    m[2][1] = sinAngle;
+    m[2][2] = cosAngle;
+    break;
 
     case 1:
       m[0][0] = cosAngle;
       m[0][1] = 0;
       m[0][2] = sinAngle;
- 
+
       m[1][0] = 0;
       m[1][1] = 1;
       m[1][2] = 0;
-			
 
-      m[2][0] = -sinAngle; 
+
+      m[2][0] = -sinAngle;
       m[2][1] = 0;
       m[2][2] = cosAngle;
       break;
-		
+
     case 2:
       m[0][0] = cosAngle;
       m[0][1] = -sinAngle;
       m[0][2] = 0;
- 
+
       m[1][0] = sinAngle;
       m[1][1] = cosAngle;
       m[1][2] = 0;
-			
+
 
       m[2][0] = 0;
       m[2][1] = 0;
       m[2][2] = 1;
       break;
     }
-	
+
   m[0][3] = 0.0;
   m[1][3] = 0.0;
   m[2][3] = 0.0;
-  m[3][0] = 0;			
+  m[3][0] = 0;
   m[3][1] = 0;
-  m[3][2] = 0;			
+  m[3][2] = 0;
   m[3][3] = 1;
-	
+
   return m;
 }
 
 Mat4d RotateEuler::GetDeriv(int dof)
 {
-  Mat4d m=vl_zero; return m;
+  // dof doesn't matter because RotateEuler only has one DOF.
+  Mat4d m;
+  int axis;
+  double cosAngle;
+  double sinAngle;
+  cosAngle = cos(mAngle);
+  sinAngle = sin(mAngle);
+  if(mAxis[0])
+    axis = 0;
+  else if(mAxis[1])
+    axis = 1;
+  else if(mAxis[2])
+    axis  = 2;
+
+  // calculate coefficients in !row! major order
+  switch(axis)
+    { case 0:
+    m[0][0] = 0;
+    m[0][1] = 0;
+    m[0][2] = 0;
+
+    m[1][0] = 0;
+    m[1][1] = -sinAngle;
+    m[1][2] = -cosAngle;
+
+    m[2][0] = 0;
+    m[2][1] = cosAngle;
+    m[2][2] = -sinAngle;
+    break;
+
+    case 1:
+      m[0][0] = -sinAngle;
+      m[0][1] = 0;
+      m[0][2] = cosAngle;
+
+      m[1][0] = 0;
+      m[1][1] = 0;
+      m[1][2] = 0;
+
+      m[2][0] = -cosAngle;
+      m[2][1] = 0;
+      m[2][2] = -sinAngle;
+      break;
+
+    case 2:
+      m[0][0] = -sinAngle;
+      m[0][1] = -cosAngle;
+      m[0][2] = 0;
+
+      m[1][0] = cosAngle;
+      m[1][1] = -sinAngle;
+      m[1][2] = 0;
+
+
+      m[2][0] = 0;
+      m[2][1] = 0;
+      m[2][2] = 1;
+      break;
+    }
+
+  m[0][3] = 0.0;
+  m[1][3] = 0.0;
+  m[2][3] = 0.0;
+  m[3][0] = 0;
+  m[3][1] = 0;
+  m[3][2] = 0;
+  m[3][3] = 1;
+
+  return m;
 }
